d0358d06906e690ade815c982e5ff4a7543d7648
Adrian Czyż <adicz5200@gmail.com>
2022-08-09 18:20:41 +0200

    [LEETCODE] 217. Contains Duplicate

--- src/main/java/leetcode/T217_ContainsDuplicate.java
+++ src/main/java/leetcode/T217_ContainsDuplicate.java
@@ -1,19 +1,18 @@
 package leetcode;
 
-
 import java.util.HashSet;
 import java.util.Set;
 
-
 /**
- * https://leetcode.com/problems/contains-duplicate/
+ * @link: https://leetcode.com/problems/contains-duplicate/
+ * @difficulty: EASY
  */
 class T217_ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> integerSet = new HashSet<>();
         for (Integer num : nums) {
             if (!integerSet.add(num)) {
-                 return true;
+                return true;
             }
         }
         return false;
--- src/test/java/leetcode/T217_ContainsDuplicateTest.groovy
+++ src/test/java/leetcode/T217_ContainsDuplicateTest.groovy
@@ -14,9 +14,9 @@ class T217_ContainsDuplicateTest extends Specification {
         result == expectedResult
 
         where:
-        nums                           | expectedResult
-        [1, 2, 3, 1]                   | true
-        [1, 1, 1, 3, 3, 4, 3, 2, 4, 2] | true
-        [1, 2, 3, 4]                   | false
+        nums                           || expectedResult
+        [1, 2, 3, 1]                   || true
+        [1, 1, 1, 3, 3, 4, 3, 2, 4, 2] || true
+        [1, 2, 3, 4]                   || false
     }
 }
