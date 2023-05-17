524fa46e236c9872dff6a141091557de6fc0272b
Adrian Czyż <adicz5200@gmail.com>
2023-05-17 23:29:20 +0200

    [LEETCODE] 1. Two Sum - second solution

--- src/main/java/leetcode/T1_TwoSum.java
+++ src/main/java/leetcode/T1_TwoSum.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.HashMap;
+
 /**
  * @link: https://leetcode.com/problems/two-sum/
  * @difficulty: EASY
@@ -11,6 +13,18 @@ public class T1_TwoSum {
                 if (nums[i] + nums[j] == target) {
                     return new int[]{i, j};
                 }
+            }
+        }
+        return null;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> integerByIndex = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (integerByIndex.containsKey(target - nums[i])) {
+                return new int[]{integerByIndex.get(target - nums[i]), i};
+            } else {
+                integerByIndex.put(nums[i], i);
             }
         }
         return null;
--- src/test/java/leetcode/T1_TwoSumTest.groovy
+++ src/test/java/leetcode/T1_TwoSumTest.groovy
@@ -1,12 +1,29 @@
 package leetcode
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class T1_TwoSumTest extends Specification {
 
     T1_TwoSum service = new T1_TwoSum()
 
-    def "should return correct value with given parameters"() {
+    @Unroll
+    def "should return correct value with given parameters - first solution"() {
+        when:
+        def result = service.twoSum(nums as int[], target)
+
+        then:
+        result == expectedResult as int[]
+
+        where:
+        nums           | target || expectedResult
+        [2, 7, 11, 15] | 9      || [0, 1]
+        [3, 2, 4]      | 6      || [1, 2]
+        [3, 3]         | 6      || [0, 1]
+    }
+
+    @Unroll
+    def "should return correct value with given parameters - second solution"() {
         when:
         def result = service.twoSum(nums as int[], target)
 
